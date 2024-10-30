187e437ea776bcca949977ca4b1da4a92155b315
axtk <axtkx@inbox.ru>
2024-10-30 23:25:09 +0300

    add more deps to server externals

--- src/types/PackageJSON.ts
+++ src/types/PackageJSON.ts
@@ -1,6 +1,11 @@
+type DependencyList = Record<string, string>;
+
 export type PackageJSON = {
     name?: string;
     version?: string;
     type?: string;
-    dependencies?: Record<string, string>;
+    dependencies?: DependencyList;
+    devDependencies?: DependencyList;
+    peerDependencies?: DependencyList;
+    optionalDependencies?: DependencyList;
 };
--- src/utils/getServerExternals.ts
+++ src/utils/getServerExternals.ts
@@ -5,11 +5,22 @@ let cachedExternals: string[] | undefined;
 
 export async function getServerExternals() {
     if (!cachedExternals) {
-        let {dependencies = {}} = await readJSON<PackageJSON>('package.json');
-        let depNames = Object.keys(dependencies);
+        let {
+            dependencies = {},
+            devDependencies = {},
+            peerDependencies = {},
+            optionalDependencies = {},
+        } = await readJSON<PackageJSON>('package.json');
+
+        let deps = new Set([
+            ...Object.keys(dependencies),
+            ...Object.keys(devDependencies),
+            ...Object.keys(peerDependencies),
+            ...Object.keys(optionalDependencies),
+        ]);
 
         let nonModuleDeps = await Promise.all(
-            depNames.map(async name => {
+            Array.from(deps).map(async name => {
                 let {type} = await readJSON<PackageJSON>(
                     `node_modules/${name}/package.json`,
                 );
