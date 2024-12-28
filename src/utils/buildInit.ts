8215b923fe65ba16b6c93901d772a5fa78428a63
axtk <axtkx@inbox.ru>
2024-12-28 13:51:25 +0300

    add fallback for failed build in buildInit

--- src/utils/buildInit.ts
+++ src/utils/buildInit.ts
@@ -7,6 +7,17 @@ import {writeModifiedFile} from './writeModifiedFile';
 
 let outputPath = 'dist/init.js';
 
+async function writeBlankInit() {
+    try {
+        await access('dist');
+    }
+    catch {
+        await mkdir('dist');
+    }
+
+    await writeFile(outputPath, '');
+}
+
 export async function buildInit() {
     let fileName = `init_${Math.random().toString(36).slice(2)}`;
     let inputPath = `src/main/${fileName}.ts`;
@@ -16,40 +27,41 @@ export async function buildInit() {
         'init/index',
     ]);
 
-    if (entryPoints.length === 0) {
+    if (entryPoints.length === 0)
+        return writeBlankInit();
+
+    try {
+        let importList = '', callList = '';
+
+        for (let i = 0; i < entryPoints.length; i++) {
+            let importPath = toImportPath(entryPoints[i].in, 'src/main');
+
+            importList += `import {init as init${i}} from '${importPath}';`;
+            callList += `        init${i}(),`;
+        }
+
+        let content = `${importList}\n\n` +
+            `(async () => {\n    await Promise.all([\n${callList}\n    ]);\n})();\n`;
+
+        await writeModifiedFile(inputPath, content);
+
+        await esbuild.build({
+            entryPoints: [inputPath],
+            bundle: true,
+            outfile: outputPath,
+            platform: 'node',
+            ...commonBuildOptions,
+        });
+
+        await unlink(inputPath);
+    }
+    catch {
         try {
-            await access('dist');
+            await access(inputPath);
+            await unlink(inputPath);
         }
-        catch {
-            await mkdir('dist');
-        }
+        catch {}
 
-        await writeFile(outputPath, '');
-
-        return;
+        await writeBlankInit();
     }
-
-    let importList = '', callList = '';
-
-    for (let i = 0; i < entryPoints.length; i++) {
-        let importPath = toImportPath(entryPoints[i].in, 'src/main');
-    
-        importList += `import {init as init${i}} from '${importPath}';`;
-        callList += `        init${i}(),`;
-    }
-
-    let content = `${importList}\n\n` +
-        `(async () => {\n    await Promise.all([\n${callList}\n    ]);\n})();\n`;
-    
-    await writeModifiedFile(inputPath, content);
-
-    await esbuild.build({
-        entryPoints: [inputPath],
-        bundle: true,
-        outfile: outputPath,
-        platform: 'node',
-        ...commonBuildOptions,
-    });
-
-    await unlink(inputPath);
 }
